8149218320531d8cd6d7395ef43f38eb39d1ddd6
Giosué Russo <josh@hizen.io>
2020-08-23 09:24:59 +0900

    Folder refactory

---
+++ AudioUVBoard/AudioUVBoard.ino
@@ -0,0 +1,44 @@
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AudioInputI2S i2s2;            //xy=509,547
+AudioAnalyzeFFT1024 fft1024_1; //xy=861,676
+AudioOutputI2S i2s1;           //xy=1019,579
+AudioConnection patchCord1(i2s2, 0, i2s1, 0);
+AudioConnection patchCord2(i2s2, 0, i2s1, 1);
+AudioConnection patchCord3(i2s2, 0, fft1024_1, 0);
+AudioControlSGTL5000 sgtl5000_1; //xy=736,779
+// GUItool: end automatically generated code
+
+void setup()
+{
+  pinMode(24, OUTPUT);
+  pinMode(25, OUTPUT);
+  pinMode(28, OUTPUT);
+
+  Serial.begin(9600);
+  AudioMemory(160);
+  sgtl5000_1.enable();
+  sgtl5000_1.volume(1);
+  sgtl5000_1.inputSelect(AUDIO_INPUT_MIC);
+  sgtl5000_1.micGain(30); // from 0db to 63db
+  delay(1000);
+}
+
+void loop()
+{
+  if (fft1024_1.available())
+  {
+    float lowBand = fft1024_1.read(0, 14);
+    float midBand = fft1024_1.read(15, 150);
+    float hiBand = fft1024_1.read(200, 511);
+
+    analogWrite(24, powf(lowBand * 5, 9) * 200 * 255);
+    analogWrite(25, powf(midBand * 20, 7) * 50 * 255);
+    analogWrite(28, powf(hiBand * 20, 8) * 500 * 255);
+  }
+}
